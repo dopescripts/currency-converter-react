b42046209e8898a54b00a2adba4a71d39a7f91e2
Minhaj Haider Shah <smhaider1808@gmail.com>
2025-06-11 13:43:09 +0500

    Implement currency conversion logic and enhance InputBox component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { InputBox } from "./components";
+import useCurrencyInfo from "./Hooks/useCurrencyInfo";
 import { RefreshCcw } from "lucide-react";
 function App() {
   const bgImage = "https://images.unsplash.com/photo-1511883040705-6011fad9edfc?q=80&w=1474&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("pkr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+
+  const options = Object.keys(useCurrencyInfo(from));
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  };
+
+  const convert = () => {
+    setConvertedAmount(Number(amount) * currencyInfo[to]);
+  };
 
   return (
     <>
@@ -23,21 +43,22 @@ function App() {
             <form
               onSubmit={(e) => {
                 e.preventDefault();
+                convert();
               }}
             >
               <div className="w-full mb-1">
-                <InputBox label="From" />
+                <InputBox label="From" amount={amount} currencyOptions={options} selectCurrency={from} onCurrencyChange={(currency) => setFrom(currency)} onAmountChange={(amount) => setAmount(amount)} />
               </div>
               <div className="relative w-full h-0.5">
-                <button type="button" className="absolute left-1/2 cursor-pointer -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5">
+                <button type="button" onClick={swap} className="absolute left-1/2 cursor-pointer -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5">
                   swap
                 </button>
               </div>
               <div className="w-full mt-1 mb-4">
-                <InputBox label="To" />
+                <InputBox label="To" disableInput amount={convertedAmount} currencyOptions={options} selectCurrency={to} onCurrencyChange={(currency) => setTo(currency)} />
               </div>
               <button type="submit" className="w-full group flex items-center justify-center gap-1 relative cursor-pointer bg-blue-600 hover:bg-blue-700 text-white px-4 py-3 rounded-lg">
-                Convert
+                Convert {from.toUpperCase()} to {to.toUpperCase()}
                 <span>
                   <RefreshCcw strokeWidth={2} size={16} className="group-hover:animate-spin" />
                 </span>
